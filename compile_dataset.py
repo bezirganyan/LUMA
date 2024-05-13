5f4664dc76c987c4309e8a27d6135a1d841ca16e
Grigor Bezirganyan <grigor.bezirganyan98@gmail.com>
2024-05-13 15:00:37 +0200

    iPerformed bug fixes

--- compile_dataset.py
+++ compile_dataset.py
@@ -3,6 +3,7 @@ import os
 
 import numpy as np
 import pandas as pd
+import torch
 from omegaconf import OmegaConf
 
 from audio_processing import add_noise_to_audio, extract_audio_deep_features, sample_audio, switch_audio_data_labels
@@ -81,12 +82,12 @@ def generate_text_modality(text_tsv_path, text_test_path, features_path, diversi
     print('[+] Text data sampled successfully!')
     if os.path.exists(text_test_path):
         print(f'[+] Test data found at {text_test_path}')
-        test_data = pd.read_pickle(text_test_path)
+        test_data = pd.read_csv(text_test_path, sep='\t')
     else:
         print(f'[-] Test data not found at {text_test_path}')
         print(f'[*] Generating test data from {text_tsv_path}')
         train_data, test_data = generate_test_split(train_data, test_count_per_label=100, features_path=features_path)
-        test_data.to_pickle(text_test_path)
+        test_data.to_csv(text_test_path, sep='\t')
         print('[+] Test data generated successfully!')
     if sample_nosie_cfg.pop('add_noise_train', False):
         print('[*] Adding noise to train data')
@@ -110,25 +111,23 @@ def generate_text_modality(text_tsv_path, text_test_path, features_path, diversi
 def generate_image_modality(image_data_path, image_test_path, features_path, diversity_cfg,
                             sample_nosie_cfg, label_switch_prob):
     print("[*] Generating image modality")
-    if not os.path.exists(image_data_path):
-        data = generate_cifar_50_data(image_data_path)
+    if not os.path.exists(image_data_path) or not os.path.exists(image_test_path):
+        data, test_data = generate_cifar_50_data(image_data_path)
     else:
         data = pd.read_pickle(image_data_path)
+        test_data = pd.read_pickle(image_test_path)
     data['class'] = data['label'].apply(lambda x: label_class_mapping[x])
+    test_data['class'] = test_data['label'].apply(lambda x: label_class_mapping[x])
     train_data = data[data['label'].isin(ID_class_labels)]
+    test_data = test_data[test_data['label'].isin(ID_class_labels)]
     if not os.path.exists(features_path):
         print(f'[*] Extracting deep features from {image_data_path}')
         features = extract_deep_image_features(data, features_path)
         print('[+] Features saved successfully!')
-    if os.path.exists(image_test_path):
-        print(f'[+] Test data found at {image_test_path}')
-        test_data = pd.read_pickle(image_test_path)
-    else:
-        print(f'[-] Test data not found at {image_test_path}')
-        print(f'[*] Generating test data from {image_data_path}')
-        train_data, test_data = generate_test_split(train_data, test_count_per_label=100, features_path=features_path)
-        test_data.to_pickle(image_test_path)
-        print('[+] Test data generated successfully!')
+
+    print(f'[*] Sampling image data from {image_data_path}')
+    train_data = sample_text(train_data, features_path, **diversity_cfg)
+    print('[+] Image data sampled successfully!')
     if sample_nosie_cfg.pop('add_noise_train', False):
         print(f'[*] Adding noise to train data')
         train_data = add_noise_to_image(train_data, **sample_nosie_cfg)
@@ -144,9 +143,11 @@ def generate_image_modality(image_data_path, image_test_path, features_path, div
         train_data = switch_image_data_labels(train_data, features_path, switch_probability=label_switch_prob)
         test_data = switch_image_data_labels(test_data, features_path, switch_probability=label_switch_prob)
         print('[+] Labels switched successfully!')
+    print('[+] Image modality generated successfully!')
+    return train_data, test_data, ood_data
 
 
-def generate_cifar_50_data(image_csv_path):
+def generate_cifar_50_data(image_csv_path, image_test_path):
     print(f'[-] Image data not found at {image_csv_path}')
     print('[*] Loading CIFAR-50 dataset')
     data_10, test_data_10, label_names_10 = load_cifar10()
@@ -163,13 +164,19 @@ def generate_cifar_50_data(image_csv_path):
     data = pd.concat([data_10, data_100, edm_data], axis=0)
     data = data[data['label'].isin(label_class_mapping.keys())]
     data = data.reset_index(drop=True)
+
+    test_data_10['source'] = 'cifar10'
+    test_data_100['source'] = 'cifar100'
+    test_data = pd.concat([test_data_10, test_data_100], axis=0)
+    test_data = test_data[test_data['label'].isin(label_class_mapping.keys())]
     # convert images to 1 dimensional array, so that it can be saved in csv file
     data_to_save = data.copy()
     data_to_save.to_pickle(image_csv_path)
+    test_data_10.to_pickle(image_test_path)
     data = pd.read_pickle(image_csv_path)
-    assert data.equals(data)
+    assert data.equals(data_to_save)
     print('[+] Image data saved successfully!')
-    return data
+    return data, test_data
 
 
 def parse_args() -> tuple[argparse.Namespace, list[str]]:
@@ -177,6 +184,45 @@ def parse_args() -> tuple[argparse.Namespace, list[str]]:
     parser.add_argument('-c', '--cfg', type=str, default='cfg/default.yml')
     args, unknown = parser.parse_known_args()
     return args, unknown
+
+
+def align_data(audio, text, image):
+    audio_chunks = []
+    text_chunks = []
+    image_chunks = []
+    for label in ID_class_labels:
+        a = audio.loc[audio['label'] == label].sample(frac=1).reset_index(drop=True)
+        t = text.loc[text['label'] == label].sample(frac=1).reset_index(drop=True)
+        i = image.loc[image['label'] == label].sample(frac=1).reset_index(drop=True)
+        audio_chunks.append(a)
+        text_chunks.append(t)
+        image_chunks.append(i)
+    audio = pd.concat(audio_chunks, axis=0).reset_index(drop=True)
+    text = pd.concat(text_chunks, axis=0).reset_index(drop=True)
+    image = pd.concat(image_chunks, axis=0).reset_index(drop=True)
+    return audio, text, image
+
+
+def align_ood_data(audio, text, image):
+    audio_label_counts = audio['label'].value_counts()
+    text_label_counts = text['label'].value_counts()
+    image_label_counts = image['label'].value_counts()
+    # get the minimum label count for each class
+    label_counts = pd.concat([audio_label_counts, text_label_counts, image_label_counts], axis=1).min(axis=1)
+    audio_chunks = []
+    text_chunks = []
+    image_chunks = []
+    for label in label_counts.index:
+        a = audio.loc[audio['label'] == label].sample(label_counts[label]).reset_index(drop=True)
+        t = text.loc[text['label'] == label].sample(label_counts[label]).reset_index(drop=True)
+        i = image.loc[image['label'] == label].sample(label_counts[label]).reset_index(drop=True)
+        audio_chunks.append(a)
+        text_chunks.append(t)
+        image_chunks.append(i)
+    audio = pd.concat(audio_chunks, axis=0).reset_index(drop=True)
+    text = pd.concat(text_chunks, axis=0).reset_index(drop=True)
+    image = pd.concat(image_chunks, axis=0).reset_index(drop=True)
+    return audio, text, image
 
 
 if __name__ == '__main__':
@@ -187,13 +233,51 @@ if __name__ == '__main__':
     audio_cfg = cfg.audio
     text_cfg = cfg.text
     image_cfg = cfg.image
-    generate_audio_modality(data_cfg.data_dir, audio_cfg.audio_csv_path, audio_cfg.audio_test_csv_path,
-                            audio_cfg.audio_data_path,
-                            audio_cfg.audio_features_path, diversity_cfg=audio_cfg.diversity,
-                            sample_nosie_cfg=audio_cfg.sample_noise, label_switch_prob=audio_cfg.label_switch_prob)
-    generate_text_modality(text_cfg.text_tsv_path, text_cfg.text_test_tsv_path, text_cfg.text_features_path,
-                           diversity_cfg=text_cfg.diversity, sample_nosie_cfg=text_cfg.sample_noise,
-                           label_switch_prob=text_cfg.label_switch_prob)
-    generate_image_modality(image_cfg.image_data_path, image_cfg.image_test_path, image_cfg.image_features_path,
-                            diversity_cfg=image_cfg.diversity,
-                            sample_nosie_cfg=image_cfg.sample_noise, label_switch_prob=image_cfg.label_switch_prob)
+    # fix the seed for reproducibility
+    if data_cfg.seed is not None:
+        np.random.seed(data_cfg.seed)
+        torch.manual_seed(data_cfg.seed)
+    audio_train, audio_test, audio_ood, = generate_audio_modality(data_cfg.data_dir, audio_cfg.audio_csv_path,
+                                                                  audio_cfg.audio_test_csv_path,
+                                                                  audio_cfg.audio_data_path,
+                                                                  audio_cfg.audio_features_path,
+                                                                  diversity_cfg=audio_cfg.diversity,
+                                                                  sample_nosie_cfg=audio_cfg.sample_noise,
+                                                                  label_switch_prob=audio_cfg.label_switch_prob)
+    if data_cfg.seed is not None:
+        np.random.seed(data_cfg.seed)
+        torch.manual_seed(data_cfg.seed)
+    text_train, text_test, text_ood = generate_text_modality(text_cfg.text_tsv_path, text_cfg.text_test_tsv_path,
+                                                             text_cfg.text_features_path,
+                                                             diversity_cfg=text_cfg.diversity,
+                                                             sample_nosie_cfg=text_cfg.sample_noise,
+                                                             label_switch_prob=text_cfg.label_switch_prob)
+    if data_cfg.seed is not None:
+        np.random.seed(data_cfg.seed)
+        torch.manual_seed(data_cfg.seed)
+    image_train, image_test, image_ood = generate_image_modality(image_cfg.image_data_path, image_cfg.image_test_path,
+                                                                 image_cfg.image_features_path,
+                                                                 diversity_cfg=image_cfg.diversity,
+                                                                 sample_nosie_cfg=image_cfg.sample_noise,
+                                                                 label_switch_prob=image_cfg.label_switch_prob)
+
+    # align the data from different modalities, so that the labels are mached and shuffled(within label)
+
+    audio_train, text_train, image_train = align_data(audio_train, text_train, image_train)
+    audio_test, text_test, image_test = align_data(audio_test, text_test, image_test)
+    audio_ood, text_ood, image_ood = align_data(audio_ood, text_ood, image_ood)
+
+    # save the data as pickle or csv or tsv file files
+    audio_train.to_csv(audio_cfg.audio_train_csv_path)
+    audio_test.to_csv(audio_cfg.audio_test_csv_path)
+    audio_ood.to_csv(audio_cfg.audio_ood_csv_path)
+
+    text_train.to_csv(text_cfg.text_train_tsv_path, sep='\t')
+    text_test.to_csv(text_cfg.text_test_tsv_path, sep='\t')
+    text_ood.to_csv(text_cfg.text_ood_tsv_path, sep='\t')
+
+    image_train.to_pickle(image_cfg.image_train_path)
+    image_test.to_pickle(image_cfg.image_test_path)
+    image_ood.to_pickle(image_cfg.image_ood_path)
+    print('[+] Data saved successfully!')
+    print('[+] Data generation completed successfully!')
